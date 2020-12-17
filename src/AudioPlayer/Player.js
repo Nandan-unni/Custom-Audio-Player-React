bc5acea6eab06efe72afb5544fdf1bf1c8f4d8d1
nandan-unni <asnqln@gmail.com>
2020-12-17 09:43:50 +0530

    Code docs

--- src/AudioPlayer/Player.js
+++ src/AudioPlayer/Player.js
@@ -7,12 +7,15 @@ import {
     } from "./Controller";
 
 function AudioPlayer(props) {
+
     const [duration, setDuration] = useState("0 : 00");
     const [currentTime, setCurrentTime] = useState("0 : 00");
     const [perc, setPerc] = useState(0);
     const [seekTime, setSeekTime] = useState(0);
+
     const audio = useRef(null);
     const seeker = useRef(null);
+
     // eslint-disable-next-line 
     useEffect(() => {
         setDuration(getTime(audio.current.duration));
@@ -27,9 +30,10 @@ function AudioPlayer(props) {
     function getSeekTime(seek) {
         setSeekTime((seek.nativeEvent.offsetX/seeker.current.offsetWidth) * 100);
     };
+
     return (
-        <div className={`AudioPlayer ${props.status}`}>
-            <div className={`control-panel ${props.status}`}>
+        <div className={`AudioPlayer`}>
+            <div className={`control-panel`}>
                 <audio id={`audio-${props.feed.pk}`} ref={audio} >
                     <source src={props.audio} />
                 </audio>
@@ -38,19 +42,19 @@ function AudioPlayer(props) {
                     id={`play-pause-${props.feed.pk}`}
                     onClick={() => playPauseController(`${props.feed.pk}`)} 
                 >
-                    play_arrow
+                        play_arrow
                 </button>
                 <button 
                     className={`material-icons`} 
                     onClick={() => rewind(`${props.feed.pk}`)} 
                 >
-                    fast_rewind
+                        fast_rewind
                 </button>
                 <button 
                     className={`material-icons`} 
                     onClick={() => forward(`${props.feed.pk}`)} 
                 >
-                    fast_forward
+                        fast_forward
                 </button>
                 <button 
                     className={`material-icons`} 
@@ -78,9 +82,9 @@ function AudioPlayer(props) {
                      ref={seeker} onMouseMove={getSeekTime.bind(this)} 
                      onClick={() => seekController(seekTime, props.feed.pk)}
                 >
-                    <div className="seeker-played" style={{width: `${perc}%`}}></div>
-                    <div className="seeker-btn" style={{marginLeft: `${perc-1.5}%`}}></div>
-                    <div className="seeker-toplay"></div>
+                        <div className="seeker-played" style={{width: `${perc}%`}}></div>
+                        <div className="seeker-btn" style={{marginLeft: `${perc-1.5}%`}}></div>
+                        <div className="seeker-toplay"></div>
                 </div>
                 <p>{ duration === "0 : 0NaN" ? "0 : 00" : duration }</p>
             </div>
